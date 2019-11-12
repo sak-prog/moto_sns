c622172e969d14aa88212cc96249b39fbef435bf
sako <sakou2019prog@gmail.com>
2019-11-13 00:19:44 +0900

    rspecの導入

--- motosns/config/application.rb
+++ motosns/config/application.rb
@@ -16,5 +16,11 @@ module Motosns
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators.test_framework = :rspec
+    config.generators.system_tests   = false
+    config.generators.stylesheets    = false
+    config.generators.javascripts    = false
+    config.generators.helper         = false
   end
 end
