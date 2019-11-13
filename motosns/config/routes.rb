2f35443f54abe0e07bad9ad3b982381dcde58b96
sako <sakou2019prog@gmail.com>
2019-11-14 00:23:53 +0900

    トップページの作成

--- motosns/config/routes.rb
+++ motosns/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'about', to: 'home#about'
+  get 'index', to: 'home#index'
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"}
 end
---
+++ motosns/spec/system/home_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Home", type: :system do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
