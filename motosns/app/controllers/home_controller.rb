3195f3215546da9011c6848f0619093257133a4f
sako <sakou2019prog@gmail.com>
2019-11-16 15:44:06 +0900

    ステータスフィードの実装

--- motosns/app/controllers/home_controller.rb
+++ motosns/app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   end
 
   def index
+    if user_signed_in?
+      @feed_items = current_user.feed.all
+    end
   end
 end
--- motosns/app/models/user.rb
+++ motosns/app/models/user.rb
@@ -27,4 +27,11 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Post.includes(:user).where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
 end
